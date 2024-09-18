33d1fd264f5e68ded05158c42b41b957bfae112b
Giuseppe Maria Sarda <giuseppe.sarda16@gmail.com>
2024-09-18 13:50:18 +0200

    refactor adder_tree_layer

--- hw/rtl/adder_tree.sv
+++ hw/rtl/adder_tree.sv
@@ -11,43 +11,44 @@
 // The output is a 32-bit value for MODE 0, and a P-bit value for MODE 1 as for this projects specific needs.
 //
 // Parameters:
-// - INPUTS_AMOUNT: number of inputs, needs to be a power of 2
-// - P: number of bits of each seperate element of the inputs
+// - NUM_INPUTS: number of inputs, needs to be a power of 2
+// - DATAW: number of bits of each seperate element of the inputs
 // - MODE: 0 for always signed addition, 1 for signed/unsigned addition according to the signedAddition input
 
-module binary_tree_adder #(
-    parameter int INPUTS_AMOUNT,
-    parameter int P,
-    parameter int MODE
+module adder_tree #(
+    parameter int NUM_INPUTS,
+    parameter int DATAW,
+    parameter int MODE,
+    // Derived
+    parameter int OUT_DATAW = DATAW + $clog2(NUM_INPUTS)
 ) (
-    input wire [P-1:0] inputs [INPUTS_AMOUNT],
+    input wire [DATAW-1:0] inputs [NUM_INPUTS],
     input wire signedAddition,
     output wire [31:0] out_32bit,
-    output wire [P+$clog2(INPUTS_AMOUNT)-1:0] out
+    output wire [OUT_DATAW-1:0] out
 
 );
 
-    if (INPUTS_AMOUNT - 1 & INPUTS_AMOUNT) $fatal("ERROR: Binary adder input not power of 2");
+    if (NUM_INPUTS - 1 & NUM_INPUTS) $fatal("ERROR: Binary adder input not power of 2");
 
-    localparam int LayerAmount = $clog2(INPUTS_AMOUNT);
+    localparam int LayerAmount = $clog2(NUM_INPUTS);
     logic signed [P+LayerAmount-1:0] temp_output ;
     generate
-        if (INPUTS_AMOUNT == 1) begin : gen_single_input
+        if (NUM_INPUTS == 1) begin : gen_single_input
             assign temp_output = inputs[0];
         end else begin : gen_tree
         genvar layer;
         for(layer = 0; layer < LayerAmount; layer = layer + 1) begin: gen_layer
-            localparam int CurrentWidth = INPUTS_AMOUNT >> layer;
-            localparam int NextWidth = INPUTS_AMOUNT >> (layer+1);
+            localparam int CurrentWidth = NUM_INPUTS >> layer;
+            localparam int NextWidth = NUM_INPUTS >> (layer+1);
             logic [P+layer:0] connectingWires [NextWidth];
             if(layer == LayerAmount-1) begin : gen_last_layer
                 assign temp_output = connectingWires[0];
             end
             if(layer == 0) begin : gen_first_layer
                 adder_tree_layer #(
-                .NUM_INPUTS(INPUTS_AMOUNT>>layer),
-                .DATAW(P),
-                .MODE(MODE)
+                .NUM_INPUTS(NUM_INPUTS>>layer),
+                .DATAW(P)
                 ) adder_tree_layer (
                     .data_i(inputs),
                     .data_o(connectingWires),
@@ -55,9 +56,8 @@ module binary_tree_adder #(
                 );
             end else begin : gen_mid_layers
                 adder_tree_layer #(
-                .NUM_INPUTS(INPUTS_AMOUNT>>layer),
-                .DATAW(P+layer),
-                .MODE(MODE)
+                .NUM_INPUTS(NUM_INPUTS>>layer),
+                .DATAW(P+layer)
                 ) adder_tree_layer (
                     .data_i(gen_layer[layer-1].connectingWires),
                     .data_o(connectingWires),
--- hw/rtl/adder_tree_layer.sv
+++ hw/rtl/adder_tree_layer.sv
@@ -14,14 +14,10 @@
 // Parameters:
 // - NUM_INPUTS: number of inputs, needs to be a power of 2
 // - DATAW: number of bits of each seperate element of the inputs
-// - MODE:
-//      0 for always signed addition
-//      1 for signed/unsigned addition according to the sign_unsign_ni input
 
 module adder_tree_layer #(
     parameter int NUM_INPUTS,
     parameter int DATAW,
-    parameter int MODE,
     // Derived
     parameter int NUM_OUTPUTS = NUM_INPUTS/2
 ) (
@@ -29,24 +25,23 @@ module adder_tree_layer #(
     output logic [DATAW:0] data_o [NUM_OUTPUTS], // #outputs = #inputs halved
     input logic sign_unsign_ni
 );
-    if (MODE==0) begin : gen_signed_tree_layer
-        genvar i;
-        for (i = 0; i < NUM_OUTPUTS; i = i + 1) begin: gen_adder
-            assign data_o[i] = signed'(data_i[2*i]) + signed'(data_i[2*i+1]);
-        end
-    end else if (MODE==1) begin : gen_unsiged_tree_layer
+    logic [DATAW:0] extd_data_i [NUM_INPUTS];
 
-        logic [DATAW:0] extd_data_i [NUM_INPUTS];
-        genvar i;
-
+    generate
         for (genvar i = 0; i < NUM_INPUTS; i = i + 1) begin: gen_extender
             assign extd_data_i[i] = sign_unsign_ni ?
                 {data_i[i][DATAW-1], data_i[i]} :   // sign extension
                 {1'b0, data_i[i]};                  // zero extension
         end
-
-        for (i = 0; i < NUM_OUTPUTS; i = i + 1) begin: gen_adder
-            assign data_o[i] = extd_data_i[2*i] + extd_data_i[2*i+1];
+        for (genvar i = 0; i < NUM_OUTPUTS; i = i + 1) begin: gen_adder
+            adder #(
+                .DATAW(DATAW+1)
+            ) adder (
+                .dataa_i(extd_data_i[2*i]),
+                .datab_i(extd_data_i[2*i+1]),
+                .sum_o(data_o[i])
+            );
         end
-    end
+    endgenerate
+
 endmodule
