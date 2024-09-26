8d23074ebf871e202d0a3bd54a51d64bea1f884e
Giuseppe Maria Sarda <giuseppe.sarda16@gmail.com>
2024-09-26 10:52:50 +0200

    add libs from ETH, added pipes to adder

--- hw/rtl/adder.sv
+++ hw/rtl/adder.sv
@@ -13,11 +13,18 @@
 // - DATAW: number of bits of the input data
 //
 // TODO:
-// - Add configurable pipeline stages
+// - Add backpressure signals
+
+`include "registers.svh"
 
 module adder #(
-    parameter int DATAW = 32
+    parameter int DATAW = 32,
+    parameter int PIPES = 0
 )(
+
+    input logic clk_i,
+    input logic rst_ni,
+
     input logic [DATAW-1:0] dataa_i,
     input logic [DATAW-1:0] datab_i,
     output logic [DATAW-1:0] sum_o
@@ -38,7 +45,18 @@ module adder #(
             sat_sum = sum;
         end
     end
-    assign sum_o = sat_sum;
+    generate
+        if (PIPES > 0) begin : g_pipe
+            logic [PIPES:0][DATAW-1:0] pipe_sum;
+            for (genvar i = 0; i < PIPES; i++) begin : g_pipe_stage
+                `FF(pipe_sum[i+1], pipe_sum[i], '0, clk_i, rst_ni);
+            end
+            assign pipe_sum[0] = sat_sum;
+            assign sum_o = pipe_sum[PIPES];
+        end else begin : g_nopipe
+            assign sum_o = sat_sum;
+        end
+    endgenerate
 
 endmodule
 
