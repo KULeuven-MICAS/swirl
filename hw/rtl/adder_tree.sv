ca2961280f54b202814b2d1f7dafb630b5d46717
quinten-guelinckx <quinten.guelinckx@student.kuleuven.be>
2024-10-23 11:57:18 +0200

    add valid and ready signals etc and logic for them

--- hw/rtl/adder_tree.sv
+++ hw/rtl/adder_tree.sv
@@ -5,6 +5,7 @@
 // Author:
 //  Mats Vanhamel <mats.vanhamel@student.kuleuven.be>
 //  Giuseppe Sarda <giuseppe.sarda16@esat.kuleuven.be
+//  Quinten Guelinckx <quinten.guelinckx@student.kuleuven.be>
 //
 // Module description:
 // Binary tree adder supporting 2^n inputs, giving 1 summed output (no overflows).
@@ -24,17 +25,27 @@ module adder_tree #(
     parameter int NUM_INPUTS,
     parameter int DATAW,
     parameter int PIPES = 0,
+    parameter int BACKPRESSURE = 0,
     // Derived
     parameter int NUM_LAYERS = $clog2(NUM_INPUTS),
     parameter int OUT_DATAW = DATAW + NUM_LAYERS
 ) (
-    input wire [DATAW-1:0] data_i [NUM_INPUTS],
-    input wire sign_unsign_ni,
-    output wire [OUT_DATAW-1:0] data_o
+    input logic clk,
+    input logic rst_n,
+
+    input logic [DATAW-1:0] data_i [NUM_INPUTS],
+    input logic sign_unsign_ni,
+    output logic [OUT_DATAW-1:0] data_o,
+
+    input logic valid_i,
+    output logic valid_o,
+
+    input logic ready_i,
+    output logic ready_o
 );
 
-    `ASSERT_INIT("Power of 2 error: NUM_INPUTS", NUM_INPUTS-1 & NUM_INPUTS);
-    `ASSERT_INIT("Not implemented error: PIPES", PIPES > 0);
+    `ASSERT_INIT(PowerOf2Error, ~((NUM_INPUTS-1) & NUM_INPUTS));
+
 
     generate
         if (NUM_INPUTS == 1) begin : gen_single_input
@@ -44,30 +55,58 @@ module adder_tree #(
                 localparam int LayerInputCnt = NUM_INPUTS >> layer;
                 localparam int NextLayerInputCnt = NUM_INPUTS >> (layer+1);
                 localparam int LayerDataW = DATAW + layer;
-                logic [LayerDataW:0] data [NextLayerInputCnt];
+                logic [LayerDataW:0] mid_data [NextLayerInputCnt];
+                logic mid_valid_o;
+                logic mid_ready_o;
 
                 if(layer == NUM_LAYERS-1) begin : gen_last_layer
-                    assign data_o = data[0];
+                    assign data_o = mid_data[0];
+                    assign valid_o = gen_layer[layer-1].mid_valid_o;
+                    assign ready_o = gen_layer[layer-1].mid_ready_o;
                 end else if(layer == 0) begin : gen_first_layer
                     adder_tree_layer #(
                         .NUM_INPUTS(NUM_INPUTS),
-                        .DATAW(DATAW)
+                        .DATAW(DATAW),
+                        .PIPES(PIPES),
+                        .BACKPRESSURE(BACKPRESSURE)
                     ) adder_tree_layer (
-                        .data_i(inputs),
-                        .data_o(data),
-                        .sign_unsign_ni(sign_unsign_ni)
+                        .clk(clk),
+                        .rst_n(rst_n),
+
+                        .data_i(data_i),
+                        .data_o(mid_data),
+                        .sign_unsign_ni(sign_unsign_ni),
+
+                        .valid_i(valid_i),
+                        .valid_o(mid_valid_o),
+
+                        .ready_i(ready_i),
+                        .ready_o(mid_ready_o)
                     );
                 end else begin : gen_mid_layers
                     adder_tree_layer #(
                         .NUM_INPUTS(NUM_INPUTS>>layer),
-                        .DATAW(LayerDataW)
+                        .DATAW(LayerDataW),
+                        .PIPES(PIPES),
+                        .BACKPRESSURE(BACKPRESSURE)
                     ) adder_tree_layer (
-                        .data_i(gen_layer[layer-1].data),
-                        .data_o(data),
-                        .sign_unsign_ni(sign_unsign_ni)
+                        .clk(clk),
+                        .rst_n(rst_n),
+
+                        .data_i(gen_layer[layer-1].mid_data),
+                        .data_o(mid_data),
+                        .sign_unsign_ni(sign_unsign_ni),
+
+                        .valid_i(gen_layer[layer-1].mid_valid_o),
+                        .valid_o(mid_valid_o),
+
+                        .ready_i(gen_layer[layer-1].mid_ready_o),
+                        .ready_o(mid_ready_o)
                     );
                 end
             end
         end
     endgenerate
+
+
 endmodule
