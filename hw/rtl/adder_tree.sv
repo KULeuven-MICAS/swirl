7b0690f51d681c5442d8e59b3773d524e513e25c
quinten-guelinckx <quinten.guelinckx@student.kuleuven.be>
2024-11-06 10:58:59 +0100

    added dynamically allocated amount of pipes

--- hw/rtl/adder_tree.sv
+++ hw/rtl/adder_tree.sv
@@ -46,6 +46,11 @@ module adder_tree #(
 
     `ASSERT_INIT(PowerOf2Error, ~((NUM_INPUTS-1) & NUM_INPUTS));
 
+    //logic to distribute PIPES across PIPESMID, PIPESBACK, PIPESFRONT in that order so PIPES = 7 gives 3,2,2 and 9 gives 3,3,3
+    localparam int PIPESBACK = PIPES / 3 + ((PIPES % 3) >> 1);
+    localparam int PIPESFRONT = PIPES / 3;
+    localparam int PIPESMID = PIPES - PIPESBACK - PIPESFRONT;
+
 
     generate
         if (NUM_INPUTS == 1) begin : gen_single_input
@@ -63,7 +68,7 @@ module adder_tree #(
                     adder_tree_layer #(
                         .NUM_INPUTS(LayerInputCnt),
                         .DATAW(LayerDataW),
-                        .PIPES(PIPES),
+                        .PIPES(PIPESBACK),
                         .BACKPRESSURE(BACKPRESSURE)
                     ) adder_tree_layer (
                         .clk_i(clk_i),
@@ -86,7 +91,7 @@ module adder_tree #(
                     adder_tree_layer #(
                         .NUM_INPUTS(NUM_INPUTS),
                         .DATAW(DATAW),
-                        .PIPES(PIPES),
+                        .PIPES(PIPESFRONT),
                         .BACKPRESSURE(BACKPRESSURE)
                     ) adder_tree_layer (
                         .clk_i(clk_i),
@@ -103,25 +108,47 @@ module adder_tree #(
                         .ready_o(mid_ready_o)
                     );
                 end else begin : gen_mid_layers
-                    adder_tree_layer #(
-                        .NUM_INPUTS(LayerInputCnt),
-                        .DATAW(LayerDataW),
-                        .PIPES(PIPES),
-                        .BACKPRESSURE(BACKPRESSURE)
-                    ) adder_tree_layer (
-                        .clk_i(clk_i),
-                        .rst_n(rst_n),
+                    if (layer != NUM_LAYERS/2) begin : gen_mid_layer_no_pipes
+                        adder_tree_layer #(
+                            .NUM_INPUTS(LayerInputCnt),
+                            .DATAW(LayerDataW),
+                            .PIPES(0),
+                            .BACKPRESSURE(BACKPRESSURE)
+                        ) adder_tree_layer (
+                            .clk_i(clk_i),
+                            .rst_n(rst_n),
 
-                        .data_i(gen_layer[layer-1].mid_data),
-                        .data_o_layer(mid_data),
-                        .sign_unsign_ni(sign_unsign_ni),
+                            .data_i(gen_layer[layer-1].mid_data),
+                            .data_o_layer(mid_data),
+                            .sign_unsign_ni(sign_unsign_ni),
 
-                        .valid_i(gen_layer[layer-1].mid_valid_o),
-                        .valid_o(mid_valid_o),
+                            .valid_i(gen_layer[layer-1].mid_valid_o),
+                            .valid_o(mid_valid_o),
 
-                        .ready_i(gen_layer[layer-1].mid_ready_o),
-                        .ready_o(mid_ready_o)
-                    );
+                            .ready_i(gen_layer[layer-1].mid_ready_o),
+                            .ready_o(mid_ready_o)
+                        );
+                    end else begin : gen_mid_layer_pipesmid
+                        adder_tree_layer #(
+                            .NUM_INPUTS(LayerInputCnt),
+                            .DATAW(LayerDataW),
+                            .PIPES(PIPESMID),
+                            .BACKPRESSURE(BACKPRESSURE)
+                        ) adder_tree_layer (
+                            .clk_i(clk_i),
+                            .rst_n(rst_n),
+
+                            .data_i(gen_layer[layer-1].mid_data),
+                            .data_o_layer(mid_data),
+                            .sign_unsign_ni(sign_unsign_ni),
+
+                            .valid_i(gen_layer[layer-1].mid_valid_o),
+                            .valid_o(mid_valid_o),
+
+                            .ready_i(gen_layer[layer-1].mid_ready_o),
+                            .ready_o(mid_ready_o)
+                        );
+                    end
                 end
             end
         end
